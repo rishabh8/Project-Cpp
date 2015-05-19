f85cde03bbd66599720f36e1d596de875917c1d5
rishabh <rishabh.parashar8@gmail.com>
2015-05-19 19:25:58 +0530

    Demo Cpp Modified

--- demo.cpp
+++ demo.cpp
@@ -8,6 +8,7 @@ class Stack
 	public:
 		int arr[10];
 		void push(int);
+		void display();
 };
 
 void Stack::push(int num)
@@ -21,3 +22,25 @@ void Stack::push(int num)
 	arr[top]=num;
 	}
 }
+
+void Stack::display()
+{
+	for(int i=0;i<top;i++)
+	{
+		cout<<arr[top]<<endl;
+	}
+}
+
+int main()
+{
+	int num;
+	Stack s;
+	
+	cout<<"Enter a number to push ";
+	cin>>num;
+	
+	s.push(num);
+	s.display();
+	return 0;
+}
+
